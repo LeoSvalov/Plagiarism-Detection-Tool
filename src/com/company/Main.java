a073a8f405f2619f5243638658ad91d824d1ec01
Lev Svalov <leos3112@gmail.com>
2020-06-22 16:08:18 +0500

    Added 1)example SomeAssignment 2)input info from the file(1st line - language, 2nd -  path) 3) also general result numbers are saved to result folder

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,8 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -12,18 +10,28 @@ public class Main {
         Runtime rt = Runtime.getRuntime();
         Scanner in = new Scanner(System.in);
         /*
-        TBD:
-            add matching postfixes for different languages:  java -> .java;  c++ --> .cpp etc
+        TODO:
+            - add matching postfixes for different languages:  java -> .java;  c++ --> .cpp etc
             (for now only java)
 
         */
-        String command = "./moss.pl -l ";
+        BufferedReader br = new BufferedReader(new FileReader("params.txt"));
+        String language = br.readLine();
+        String path = br.readLine();
+        /* TODO:
+            - validation on error in input in params.txt
+            - ability to pass threshold number (optionally, otherwise - some default value)
+        */
+        System.out.println("No errors with input.");
+        /*
         // here reading
         System.out.println("Input the language:");
         String language = in.next();
         System.out.println("Input the absolute path to the solutions:");
         String path = in.next();
-//        String path =  "/Users/levsvalov/IdeaProjects/PlagiarismDetectionTool/SomeAssignment";
+        String path =  "/Users/levsvalov/IdeaProjects/PlagiarismDetectionTool/SomeAssignment";
+        */
+        String command = "./moss.pl -l ";
         File file = new File(path);
         File[] files = file.listFiles();
         command = command +  language + " -d ";
@@ -31,36 +39,51 @@ public class Main {
             File[] a = files[i].listFiles();
             if (a!=null) {
                 for (int j = 0; j < a.length; j++) {
-                    if (a[j].getName().endsWith(".java")) { // here dictionary is needed for postfixes for appropriate language
+                    if (a[j].getName().endsWith(".java")) {
+                        // here dictionary is needed for postfixes for appropriate language
 
+
+                        command += path + "/" + files[i].getName() + "/" + a[j].getName() + " ";
                     }
-                    command += path + "/" + files[i].getName() + "/" + a[j].getName() + " ";
                 }
             }
         }
-        System.out.println(command);
+
+
+//        System.out.println(command);
         Process proc = rt.exec(command);
         BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
         BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
         String s = null;
         String urlMoss = "";
+
         while ((s = stdInput.readLine()) != null) {
-            System.out.println(s);
-//            urlMoss =  s;
+            urlMoss =  s;
         }
-        System.out.println("-------------");
 
         String commandJplag = "java -jar jplag-2.11.8-SNAPSHOT-jar-with-dependencies.jar -l java17 -s " +  path;
         proc = rt.exec(commandJplag);
         stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        ArrayList<String> arrJplag = new ArrayList<>();
         while ((s = stdInput.readLine()) != null) {
-            System.out.println(s);
+            if(s.startsWith("Comparing")){
+                arrJplag.add(s);
+            }
         }
-
-       /* System.out.println("Here is the standard error of the command (if any):\n");
-        while ((s = stdError.readLine()) != null) {
-            System.out.println(s);
-        }*/
-
+        FileWriter wr = new FileWriter(new File("result", "indexGeneral.txt"));
+        wr.write(urlMoss + "\n");
+        wr.write("\n");
+        for (String a: arrJplag ){
+            wr.write(a+"\n");
+        }
+        wr.close();
+        System.out.println("The results are stored in folder /result/");
+        /*
+        System.out.println("General numbers are here:");
+        System.out.println(urlMoss);
+        for (String a: arrJplag ){
+            System.out.println(a);
+        }
+        */
     }
 }
